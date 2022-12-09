d2f5e88b61a06cb915faa288d5a65af2fb50173c
Letitia Ho <letitiayhho@uchicago.edu>
2022-12-09 22:30:58 +0000

    Combine into one play_tone() function with 'sound' as a boolean argument

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
 from psychtoolbox import GetSecs, WaitSecs
 import numpy as np
+import random
 
 
 # --- Constants ---
@@ -40,59 +41,33 @@ def fixation(WIN, secs):
     fixation = visual.TextStim(WIN, '+')
     fixation.draw()
     WIN.flip()
-    WaitSecs(secs)
+    jitter = random.uniform(-0.1, 0)
+    WaitSecs(secs + jitter)
     WIN.flip()
     return(fixation)
 
-def play_tone(TONE_DUR, ISI, freq, mark):
-    play_sound_with_cue(WIN, TONE_DUR, freq)
-    MARKER.send(mark)
+def play_tone(WIN, TONE_DUR, freq, sound = True):
+    now = GetSecs()
+    snd = Sound(freq, secs = TONE_DUR)
+    prompt = visual.TextStim(WIN, '*')
+    prompt.draw()
+    snd.play(when = now + 0.001) if sound else None
+    WaitSecs(0.001)
+    WIN.flip()
 
-#     # Prepare sound and visuals
-#     snd = Sound(freq, secs = TONE_DUR)
-#     prompt = visual.TextStim(WIN, '*')
-#     prompt.draw()
-    
-#     # Play sound
-#     now = GetSecs()
-#     snd.play(when = now + 0.001)
-
-#     # Send marker and update display when tone starts to play
-#     WaitSecs(0.001)
+# def play_tone(TONE_DUR, ISI, freq, mark):
+#     play_tone_with_cue(WIN, TONE_DUR, freq)
 # #     MARKER.send(mark)
-#     WIN.flip()
-    
-#     # Turn off visuals after tone is finished playing
-#     WaitSecs(TONE_DUR)
-#     WIN.flip()
-    
-    # Wait for ISI
-    WaitSecs(ISI)
+#     WaitSecs(ISI)
 
-def imagine_tone(WIN, TONE_DUR, ISI, mark):
-    freq = 0
-    play_sound_with_cue(WIN, TONE_DUR, freq)
-    MARKER.send(mark)
-
-#     # Prepare visuals
-#     prompt = visual.TextStim(WIN, '*')
-#     prompt.draw()
-#     now = GetSecs()
-
-#     # Prompt subject to imagine tone
-#     WaitSecs(0.001)
+# def imagine_tone(WIN, TONE_DUR, ISI, mark):
+#     freq = None
+#     play_tone_with_cue(WIN, TONE_DUR, freq)
 # #     MARKER.send(mark)
-#     WIN.flip()
+#     WaitSecs(ISI)
     
-#     # Turn off visuals 
-#     WaitSecs(TONE_DUR)
-#     WIN.flip()
-    
-    # Wait for ISI
-    WaitSecs(ISI)
-    
-def get_mark(index, heard):
-    if heard: # if tone was heard, marker starts with '1'
+def get_mark(index, sound):
+    if sound: # if tone was played, marker starts with '1'
         mark = int(str(1) + str(index + 1))
     else: # if tone was imagined, marker starts with '2'
         mark = int(str(2) + str(index + 1))
@@ -106,15 +81,19 @@ def get_trial(FREQS, TONE_DUR, ISI, WIN, TONES_PER_TRIAL):
     marks = []
     
     for i in range(TONES_PER_TRIAL):
-        mark = get_mark(index, heard = True)
-        play_tone(TONE_DUR, ISI, freq, mark)
+        mark = get_mark(index, sound = True)
+        play_tone(TONE_DUR, ISI, freq, mark, sound = True)
+#         MARKER.send(mark)
+        WaitSecs(ISI)
         
         freqs.append(freq)
         marks.append(mark)
 
     for i in range(TONES_PER_TRIAL):
-        mark = get_mark(index, heard = False)
-        imagine_tone(WIN, TONE_DUR, ISI, mark)
+        mark = get_mark(index, sound = False)
+        imagine_tone(WIN, TONE_DUR, ISI, mark, sound = False)
+#         MARKER.send(mark)
+        WaitSecs(ISI)
         
         freqs.append(freq)
         marks.append(mark)
@@ -122,6 +101,8 @@ def get_trial(FREQS, TONE_DUR, ISI, WIN, TONES_PER_TRIAL):
     return(freq, freqs, marks)
 
 def white_noise(secs):
+    jitter = random.uniform(-0.1, 0);
+    return(none)
     # jitter
     # play white noise for jittered duration
     
@@ -129,19 +110,7 @@ def play_displaced_target(WIN, TONE_DUR, freq):
     # Prepare sound
     displacement = np.random.randint(-10, 10)
     displaced_freq = freq + displacement
-    play_sound_with_cue(WIN, TONE_DUR, displaced_freq)
-#     snd = Sound(displaced_freq, secs = TONE_DUR)
-    
-#     # Prepare visuals
-#     prompt = visual.TextStim(WIN, '*')
-#     prompt.draw()
-    
-#     # Play sound
-#     now = GetSecs()
-#     snd.play(when = now + 0.001)
-#     WaitSecs(0.001)
-#     WIN.flip()
-    
+    play_tone(WIN, TONE_DUR, displaced_freq)
     return(displayed_freq)
     
 def pitch_adjustment(WIN, TONE_DUR, displaced_freq):
@@ -156,25 +125,16 @@ def pitch_adjustment(WIN, TONE_DUR, displaced_freq):
         elif keys:
             if 'up' in keys:
                 displaced_freq += 1
-                play_sound_with_cue(WIN, TONE_DUR, displaced_freq)
+                play_tone(WIN, TONE_DUR, displaced_freq)
             elif 'down' in keys:
                 displaced_freq -= 1
-                play_sound_with_cue(WIN, TONE_DUR, displaced_freq)
+                play_tone(WIN, TONE_DUR, displaced_freq)
 
     response = displaced_freq
     return(response)
 
-
-def play_sound_with_cue(WIN, TONE_DUR, freq):
-    now = GetSecs()
-    snd = Sound(freq, secs = TONE_DUR)
-    prompt = visual.TextStim(WIN, '*')
-    prompt.draw()
-    snd.Play(when = now + 0.001)
-    WaitSecs(0.001)
-    WIN.flip()
-
 def feedback(freq, response):
+    return(none)
     # display feedback
 
 
@@ -185,7 +145,7 @@ for i in range(3):
     WaitSecs(1)
     fixation(WIN, 2)
     WaitSecs(1)
-    freq, freqs, marks = get_trial(FREQS, TONE_DUR, ISI, WIN)
+    freq, freqs, marks = get_trial(FREQS, TONE_DUR, ISI, WIN, TONES_PER_TRIAL)
     WaitSecs(1)
     white_noise(1)
     WaitSecs(1)

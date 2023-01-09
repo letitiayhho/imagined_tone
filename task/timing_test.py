28a50c160c1527a90201e226260093e08b22e68a
John <johnv@uchicago.edu>
2023-01-09 11:15:50 -0600

    Get timing test working

--- task/timing_test.py
+++ task/timing_test.py
@@ -1,17 +1,22 @@
+import random
 from functions import *
+from events import EventMarker
+
 FREQS = [190, 280]
 TONE_DUR = 0.4
 ISI = 0.4
 N_TONES = 1000
 
-# MARKER = EventMarker()
-MARKER = None
+MARKER = EventMarker()
+# MARKER = None
 
-freq = FREQS[random.randint(0, 1)]
+index = random.randint(0,1)
+freq = FREQS[index]
 for i in range(1000):
+    print(i)
     mark = get_mark(index, sound = True)
     snd = Sound(freq, secs = TONE_DUR)
-    snd.play(when = now + 0.001)
+    snd.play()
     WaitSecs(0.001)
     MARKER.send(mark)
     WaitSecs(TONE_DUR)
